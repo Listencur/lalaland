e3aa48ed5b6e07ef964ae9c1eb82b6126cf4986d
Listencur <y2319447830@gmail.com>
2023-12-05 11:05:51 +0800

    first-change

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const SITE = 'https://jsun.lol';
+export const HEADER_IMAGE = SITE + '/header.png';
+export const SITE_NAME = 'Three Kids';
+export const TWITTER_ACCOUNT = '@LangS';
+export const DESCRIPTION =
+  "yoo..man,Whats up! Welcome to LangS's personal website!";
